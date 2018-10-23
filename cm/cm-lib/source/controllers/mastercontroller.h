1c3282c311b1800f926085248a3144dde6a63f5c
Ahumbawapa <ahumbawapa@gmx.net>
2018-10-23 15:10:08 +0200

    added public field and QProperty for welcome message to master controller

--- cm/cm-lib/source/controllers/mastercontroller.h
+++ cm/cm-lib/source/controllers/mastercontroller.h
@@ -2,6 +2,7 @@
 #define MASTERCONTROLLER_H
 
 #include <QtCore/QObject>
+#include <QString>
 
 #include <cm-lib_global.h>
 
@@ -12,8 +13,10 @@ namespace controllers {
 class CMLIBSHARED_EXPORT MasterController : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString ui_welcomeMessage MEMBER welcomeMessage CONSTANT)
 public:
     explicit MasterController(QObject *parent = nullptr);
+    QString welcomeMessage = "This is MasterController to Major Tom";
 
 signals:
 
