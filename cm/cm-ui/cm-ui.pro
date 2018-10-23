0823d9b8e29fff85c61a332409accf2d8ac52136
Ahumbawapa <ahumbawapa@gmx.net>
2018-10-23 12:13:22 +0200

    using mastercontroller in main.cpp and registerd to qml engine modified cm-ui.pro for use of mastercontroller in cm-lib

--- cm/cm-ui/cm-ui.pro
+++ cm/cm-ui/cm-ui.pro
@@ -13,12 +13,16 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-INCLUDEPATH += source
+INCLUDEPATH += source \
+        ../cm-lib/source
 
 SOURCES += \
         source/main.cpp
 
 RESOURCES += views.qrc
+
+LIBS += -L$$PWD/../../build-cm-Desktop_Qt_5_11_1_MinGW_32bit-Debug/cm-lib/debug -lcm-lib
+
 
 # Additional import path used to resolve QML modules in Qt Creator's code model
 QML_IMPORT_PATH = $$PWD
--- cm/cm-ui/source/main.cpp
+++ cm/cm-ui/source/main.cpp
@@ -1,5 +1,8 @@
 #include <QtGui/QGuiApplication>
 #include <QtQml/QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include <controllers/mastercontroller.h>
 
 int main(int argc, char *argv[])
 {
@@ -7,7 +10,14 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    //register qml types
+    qmlRegisterType<cm::controllers::MasterController>("CM", 1, 0, "MasterController");
+
+    //create instance of Mastercontroller
+    cm::controllers::MasterController masterController;
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("masterController", &masterController);
     engine.load(QUrl(QStringLiteral("qrc:/views/MasterView.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
